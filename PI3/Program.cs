e6f278d50364b8abf011452953476c3e60a995a7
Raulzip <raulsantosferreira8@gmail.com>
2023-06-14 18:09:58 -0300

    Criação do Edit Alteração em alguns componentes

--- PI3/Program.cs
+++ PI3/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Contexto>(
-    option => option.UseSqlServer("Server=C3PO;Database=PI3; Trusted_Connection=True; Encrypt=True; TrustServerCertificate=true;")
+    option => option.UseSqlServer("Server=DESKTOP-BK7MN5F;Database=PI3; Trusted_Connection=True; Encrypt=True; TrustServerCertificate=true;")
     );
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
